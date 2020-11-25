e58fb5a87dce10048091d529686a3fbebc890f32
munroe1786 <era3munroe@gmail.com>
2020-11-25 09:13:31 -0600

    add imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
@@ -9,6 +12,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//set up our store
+//any component we wrap in provider will have access
+//to our redux store
+
+//need middleware to incorporate thunk with our store
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
