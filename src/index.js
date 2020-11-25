25a5d9f818ed86087515845b2ba052daeec6cfef
munroe1786 <era3munroe@gmail.com>
2020-11-25 10:00:18 -0600

    add store and composeEnhancers for dev tools to app.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  reducer, 
+  composeEnhancers(applyMiddleware(thunk))
+)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
+//store is where we store data globally
+
+//we will dispatch(send) action objects to our reducer
+
+//reducer will take in action object and decide, 
+//based on conditions in the reducer,
+//what we want to update about our current store 
+//and return a new version of the store
+
+//pass store in as a prop
+
+//compose comes from redux, allows us to combine a couple 
+//of different middlewares into one so that we can pass one argument
+//as the second argument of createStore
+
+//Provider gives access to store to App and its
+//child components
+
 //set up our store
+
 //any component we wrap in provider will have access
 //to our redux store
 
