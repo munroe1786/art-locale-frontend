b11dc82c2c24b9e7d7400a0cc481b3ff95863c66
munroe1786 <era3munroe@gmail.com>
2020-11-25 12:43:20 -0600

    revise import spacing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
@@ -8,9 +8,7 @@ import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(
-  reducer, 
-  composeEnhancers(applyMiddleware(thunk))
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk))
 )
 
 ReactDOM.render(
