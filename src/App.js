33f8e21bc0f973971465e06919df7fbeb9abe9a8
munroe1786 <era3munroe@gmail.com>
2020-11-25 08:03:01 -0600

    revise test fetch in app / .eslintcache system automated change

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import React from 'react';
 class App extends React.Component {
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/v1/artists')
+    fetch('http://localhost:3000/api/v1/artists/1')
     .then(response => response.json()) 
-    .then(data => console.log(data[0])) 
+    .then(data => console.log(data)) 
   }
 
     //return from fetch is response
