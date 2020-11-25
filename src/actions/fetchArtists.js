6a76804d7b98521fa25b36bf385330fc96a32b21
munroe1786 <era3munroe@gmail.com>
2020-11-25 14:16:32 -0600

    create fetchArtists action

---
+++ src/actions/fetchArtists.js
@@ -0,0 +1,11 @@
+export function fetchArtists() {
+
+}
+
+//action creator is just a function
+//we dispatch an action 
+//an action creator creates an action
+//object and then that action object will 
+//be dipatched to our reducer which will then
+//return our new version of the state
+//based on the action that we set
