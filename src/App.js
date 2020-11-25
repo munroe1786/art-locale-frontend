3e67e50d5097208193971722177e789c1e9ca566
munroe1786 <era3munroe@gmail.com>
2020-11-24 19:30:38 -0600

    add componentDidMount to App for testing only

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
+
+
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/artists')
+    .then(response => response.json()) 
+    .then(data => console.log(data[0])) 
+  }
+
+    //return from fetch is response
+    //data is the json version of response and is the arg here
+    //thens won't complete until the previous request is completed
+    //when you chain thens, the return value of the previous one is the argument
+    //in the following one
+
+  //fetch request - GET is default request type
+
   render () {
     return (
       <div className="App">
