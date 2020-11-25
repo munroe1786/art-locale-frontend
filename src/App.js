986ac0ed8d235f2882f06be965528a1bbe4aad59
munroe1786 <era3munroe@gmail.com>
2020-11-25 15:15:08 -0600

    add componentDidMount to App.js for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/artists')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+  
+  
+  
   render () {
     return (
       <div className="App">
