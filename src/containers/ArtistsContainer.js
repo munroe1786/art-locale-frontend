1ade6047ca0ad5daeea017074158cfe062121a5f
munroe1786 <era3munroe@gmail.com>
2020-11-26 07:37:10 -0600

    add Artists and ArtistInput render to ArtistsContainer and add notes for understanding process

--- src/containers/ArtistsContainer.js
+++ src/containers/ArtistsContainer.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import Artists from '../components/Artists';
+import ArtistInput from '../components/ArtistInput';
+import {connect} from 'react-redux';
 
 class ArtistsContainer extends React.Component {
 
     render() {
         return(
             <div>
-                ArtistsContainer
+                <ArtistInput />
+                <Artists />
             </div>
         )
     }
 
 }
 
-export default ArtistsContainer;
+export default connect() (ArtistsContainer);
 
 //makes sense that this container
 //will contain other components  
 //that have to do with Artists
+
+//order of things now 11/26/20 -
+    //attach app to the DOM
+    //app renders artist container
+    //artist container renders 
+        //Artists and ArtistInput
+
+
+//if we want to fetch artists from our
+//backend, add them to redux store
+//and use those artists from our
+//backend---need to connect to the store
