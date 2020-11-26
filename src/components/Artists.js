58258b8270d698eb6d3681387b2d69d44a329e2f
munroe1786 <era3munroe@gmail.com>
2020-11-26 07:17:23 -0600

    build out Artists component

---
+++ src/components/Artists.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Artists = (props) => {
+
+    return(
+        <div>
+            Artists
+        </div>
+
+    )
+}
+
+export default Artists;
+
+
+//this component will get props from 
+//the Artist Container
+
+//since it;s a func component we have 
+//to pass them in as arguments
+//pass in as props or deconstruct them 
+//to pull out what we need
+
+//don't need a render in func components
+
+
