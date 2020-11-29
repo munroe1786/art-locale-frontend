762c1cc1cd2b512bc4e8bbcf2121370a8543eb7c
munroe1786 <era3munroe@gmail.com>
2020-11-29 10:50:13 -0600

    add local state to ArtistInput component and add values to input form

--- src/components/ArtistInput.js
+++ src/components/ArtistInput.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 class ArtistInput extends React.Component {
 
+    state = {name: '', style: ''}
+
     render() {
         return (
             <div>
                 <form>
                     <label>Artist Name:</label>
-                        <input type='text' placeholder='Name' /><br/>
+                        <input type='text' 
+                        placeholder='Name' 
+                        value={this.state.name}
+                        /><br/>
                     <label>Style:</label>
-                        <input type='text' placeholder='Style' />
+                        <input type='text' 
+                        placeholder='Style'
+                        value={this.state.style} />
                 </form>
             </div>
         )
@@ -18,24 +25,22 @@ class ArtistInput extends React.Component {
 
 export default ArtistInput;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//need a controlled form
+//uncontrolled form - the elements float
+//around in the DOM
+//add values so that the component
+//has control over the value of each input
+//add state through a constructor or
+//through state = to make this a controlled
+//form
 
 //in order to have a controlled form, you 
-//can use local state or have form 
+//can use local state directly
+//from the form itself or have form 
 //data available in the redux store
+
+//reason for it being in redux store
+//if you're resuing the same form for creating
+//and editing - might make sense to save those
+//values in your redux store so that you
+//can reuse that component
