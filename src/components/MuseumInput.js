c4c05640ad40d0219fcd58a51cb3c0572662f0f7
munroe1786 <era3munroe@gmail.com>
2020-12-01 17:03:07 -0600

    start building museum input form

--- src/components/MuseumInput.js
+++ src/components/MuseumInput.js
@@ -1,15 +1,52 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class MuseumInput extends React.Component {
+    state = {
+        name: '',
+        location: '',
+        description: ''
+    }
 
+    handleChange = () => {
+
+    }
 
     render() {
         return (
             <div>
-                Museum Input
+                <form>
+                    <label>Museum Name:</label>
+                        <input type="text"
+                            name="name"
+                            value={this.state.name}
+
+
+                            />
+                    <label>Museum Location:</label>
+                        <input type="text"
+                            name="location"
+                            value={this.state.location}
+                            
+                            />
+                    <label>Museum Description:</label>
+                        <input type="text"
+                            name="description"
+                            value={this.state.description}
+                            
+                            />
+                </form>
             </div>
         )
     }
 }
 
-export default MuseumInput;
+export default connect(null, )(MuseumInput);
+
+//doesn;t need mapStateToProps because props are
+//coming down from MuseumsContainer
+//
+
+//controlled form can mean values assigend to your input 
+//options are controlled
+//by local state or the redux store
