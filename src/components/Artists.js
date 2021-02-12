e3219bbc560474a07a37f10081a29fdeff364257
munroe1786 <era3munroe@gmail.com>
2021-02-12 17:16:25 -0600

    add HeartToggle Component and render it in Artists component

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Artist from './Artist';
+import HeartToggle from './HeartToggle';
 
 const Artists = (props) => {
     
     return(
         <div>
             <h3>Welcome to Art Locale</h3>
+    
             <h4>Click an Artist to see which museums display their art</h4>
             {props.artists.map(artist => 
                 <li key={artist.id}>
-                <Link to={`/artists/${artist.id}`}>{artist.name}</Link>
-    
+                <Link to={`/artists/${artist.id}`}>{artist.name}
+                    </Link>
+                <HeartToggle />
                 </li>
+                
             )}
         </div>
 
